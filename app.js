3a2204e8a43f0473e10f9c0d618137ff7d96c6fa
庄再茜 <zqzhuang888@xkeshi.com>
2017-12-28 11:29:00 +0800

    modified

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 const http = require('http')
-
-const homePage = `
-<!DOCTYPE html>
-<html>
-  <style>
-    body {
-      background-color: '#ddd'
-    }
-  </style>
-  <head>
-    <meta charset="utf-8">
-    <title>Node</title>
-  </head>
-  <body>
-    <h1>
-      hello test
-    </h1>
-  </body>
-</html>
-`
+const fs = require('fs')
+const data = fs.readFileSync('./index.html')
+// const homePage = `
+// <!DOCTYPE html>
+// <html>
+//   <style>
+//     body {
+//       background-color: '#ddd'
+//     }
+//   </style>
+//   <head>
+//     <meta charset="utf-8">
+//     <title>Node</title>
+//   </head>
+//   <body>
+//     <h1>
+//       hello test
+//     </h1>
+//   </body>
+// </html>
+// `
 
 http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end(homePage)
+  res.end(data)
 })
-.listen(3000, () => {
-  console.log('Server Running At 3000')
+.listen(20188, () => {
+  console.log('Server Running At 20188')
 })
